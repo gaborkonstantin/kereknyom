394627e9b94326cfcd089fcdcb843bb7c00f22c1
gabor <gaborkonstantin@gmail.com>
2025-05-29 05:11:31 +0200

    Add structure to Kereknyom App

---
+++ src/main/java/com/spring/kereknyom/KereknyomApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kereknyom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KereknyomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KereknyomApplication.class, args);
+	}
+
+}
